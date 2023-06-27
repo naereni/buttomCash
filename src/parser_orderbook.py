27738ac5a236fd019d8b1390342eedd79823d97c
naereni <naereni12@gmail.com>
2023-06-27 05:23:27 +0300

    Create def for collect data from binance

--- src/parser_orderbook.py
+++ src/parser_orderbook.py
@@ -14,7 +14,7 @@ FIRST_LINE = "open_time,open,high,low,close,volume,close_time,quote_volume,\
 
 def get_missing_dates(start_date: str) -> list[str]:
     today = datetime.today().strftime("%Y-%m-%d")
-    miss_dates = pd.date_range(start=start_date, end=today).to_list()
+    miss_dates = pd.date_range(start=start_date, end=today, inclusive='left').to_list()
     miss_dates = [date.strftime("%Y-%m-%d") for date in miss_dates]
     return miss_dates
 
@@ -22,21 +22,30 @@ def get_missing_dates(start_date: str) -> list[str]:
 def collect_orderbook(
     save_dir: str,
     mode: str = "klines",
-    symbol: str = "BTCUSDT",
-    timing: str = "1m",
+    symbol: str = "LTCUSDT",
+    timing: str = "4h",
     start_date="2020-01-01",
 ) -> None:
-    collect_df = pd.DataFrame(
-        columns=[column for column in FIRST_LINE.split(",")]
-    )
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+        collect_df = pd.DataFrame(
+            columns=[column for column in FIRST_LINE.split(",")]
+        )
+    else:
+        collect_df = pd.read_csv(f"{save_dir}/{symbol}-{timing}.csv")
+        start_date = collect_df.iloc[-1]['date']
     missing_dates = get_missing_dates(start_date)
     for date in tqdm(missing_dates):
         download_url = (
             f"{BASE_URL}/{mode}/{symbol}/{timing}/{symbol}-{timing}-{date}.zip"
         )
         locale_name = f"{save_dir}/{symbol}-{timing}-{date}"
-
-        wget.download(url=download_url, out=save_dir)
+        try:
+            wget.download(url=download_url, out=save_dir)
+        except:
+            collect_df.to_csv(f"{save_dir}/{symbol}-{timing}.csv")
+            break
+            
         with zipfile.ZipFile(f"{locale_name}.zip", "r") as zip_ref:
             zip_ref.extractall(save_dir)
 
